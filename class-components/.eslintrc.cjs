2bdc419690e6717b7b59c42987189941d4e84e50
Volha Melayok <neiwra@gmail.com>
2024-07-06 14:07:56 +0200

    feat: add eslint and prettier

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -5,14 +5,23 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-compiler', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
