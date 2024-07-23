032a26b201141983a60f369ab84657480a7b9b54
Volha Melayok <neiwra@gmail.com>
2024-07-23 18:49:38 +0200

    feat: create slice

--- src/interfaces/pokemons.ts
+++ src/interfaces/pokemons.ts
@@ -1,3 +1,5 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
 export interface IPokemons {
   count: number;
   next: string;
@@ -16,3 +18,41 @@ export interface IPokemon {
   order: number;
   id: number;
 }
+
+interface PokemonState {
+  data: IPokemon[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: PokemonState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState,
+  reducers: {
+    fetchPokemonsStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchPokemonsSuccess(state, action: PayloadAction<IPokemon[]>) {
+      state.loading = false;
+      state.data = action.payload;
+    },
+    fetchPokemonsFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchPokemonsStart,
+  fetchPokemonsSuccess,
+  fetchPokemonsFailure,
+} = pokemonSlice.actions;
+export default pokemonSlice.reducer;
